9063e42e60ede35d17d1facf58e7f1c2ce8b8ef6
Alessandro Holanda <alessandro.holanda@hotmail.com>
2014-11-21 18:32:33 -0200

    add component and material

---
+++ src/main/java/br/com/altamira/data/dao/common/MaterialDao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.altamira.data.dao.common;
+
+import br.com.altamira.data.dao.BaseDao;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Alessandro
+ */
+@Stateless
+public class MaterialDao extends BaseDao<br.com.altamira.data.model.common.Material> {
+    
+}

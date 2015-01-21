f4f1f7a05297f60625bf9ac971452ac9200151b4
Alessandro Holanda <alessandro.holanda@hotmail.com>
2015-01-21 09:52:29 -0200

    fix shipping planning to get entire list of BOM -> Item -> Component -> Delivery in one request

--- src/main/java/br/com/altamira/data/dao/shipping/planning/BOMDao.java
+++ src/main/java/br/com/altamira/data/dao/shipping/planning/BOMDao.java
@@ -1,9 +1,21 @@
 package br.com.altamira.data.dao.shipping.planning;
 
 import br.com.altamira.data.dao.BaseDao;
+import br.com.altamira.data.model.shipping.planning.Component_;
+import br.com.altamira.data.model.shipping.planning.Delivery;
+import br.com.altamira.data.model.shipping.planning.Item;
 import br.com.altamira.data.model.shipping.planning.BOM;
+import br.com.altamira.data.model.shipping.planning.BOM_;
+import br.com.altamira.data.model.shipping.planning.Component;
+import br.com.altamira.data.model.shipping.planning.Item_;
 
 import javax.ejb.Stateless;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Root;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.core.MultivaluedMap;
 
 /**
  * Sales bom persistency strategy
@@ -11,6 +23,31 @@ import javax.ejb.Stateless;
  */
 @Stateless(name = "shipping.planning.BOMDao")
 public class BOMDao extends BaseDao<BOM> {
+
+    /**
+     *
+     * @param parameters
+     * @return
+     */
+    /*@Override
+    public CriteriaQuery<BOM> getCriteriaQuery(@NotNull MultivaluedMap<String, String> parameters) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<BOM> criteriaQuery = cb.createQuery(BOM.class);
+
+        Root<BOM> bom = criteriaQuery.from(BOM.class);
+        Join<BOM, Item> item = bom.join(BOM_.item);
+        Join<Item, Component> component = item.join(Item_.component);
+        Join<Component, Delivery> delivery = component.join(Component_.delivery);
+        
+        bom.fetch(BOM_.item);
+        //item.fetch(Item_.component);
+        //component.fetch(Component_.delivery);
+        //delivery.fetch(Delivery.class);
+
+        criteriaQuery.select(bom);
+
+        return criteriaQuery;
+    }*/
 
     /**
      *
@@ -25,6 +62,7 @@ public class BOMDao extends BaseDao<BOM> {
                 item.getComponent().size();
                 item.getComponent().stream().forEach((component) -> {
                     component.getMaterial().setComponent(null);
+                    component.getDelivery().size();
                 });
             });
         }

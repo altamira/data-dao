0fdc1127cb90a5198b6e5afb569f40494dc230a1
parthpatel-tatvasoft <parth.patel@tatvasoft.com>
2015-02-02 15:14:09 +0530

    ALTAMIRA-22: Improved code

--- src/main/java/br/com/altamira/data/dao/shipping/planning/ComponentDao.java
+++ src/main/java/br/com/altamira/data/dao/shipping/planning/ComponentDao.java
@@ -6,8 +6,10 @@
 package br.com.altamira.data.dao.shipping.planning;
 
 import java.math.BigDecimal;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 import br.com.altamira.data.dao.BaseDao;
@@ -20,6 +22,7 @@ import br.com.altamira.data.model.shipping.planning.Delivery;
 import javax.ejb.Stateless;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Root;
 
 /**
@@ -43,129 +46,41 @@ public class ComponentDao extends BaseDao<Component> {
         
         /* ALTAMIRA-22: Shipping Planning - amount remaining calculation */
         
-        // Retrieve the set of delivered items
+        // Retrieve the amount of delivered item quantities
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-    	CriteriaQuery<Delivered> criteria = builder.createQuery(Delivered.class);
+    	CriteriaQuery<BigDecimal> criteria = builder.createQuery(BigDecimal.class);
     	Root<Delivered> root = criteria.from(Delivered.class);
-    	criteria.select(root);
+    	Expression<BigDecimal> sum = builder.sum(root.get("quantity").get("value"));
+    	criteria.select(sum);
     	criteria.where(builder.equal(root.get("component").get("id"), entity.getId()));
-    	List<Delivered> deliveredList = entityManager.createQuery(criteria).getResultList();
+    	BigDecimal amt_delivered = entityManager.createQuery(criteria).getSingleResult();
+    	amt_delivered = (amt_delivered==null) ? BigDecimal.ZERO : amt_delivered;
 
     	Unit unitEntity = entity.getQuantity().getUnit();
+
+    	Measure measureEntity = new Measure();
+    	measureEntity.setValue(amt_delivered);
+    	measureEntity.setUnit(unitEntity);
+    	entity.setDelivered(measureEntity);
+
+    	Set<Delivery> deliverySet = entity.getDelivery();
+    	TreeSet<Delivery> treeSet = new TreeSet<Delivery>(deliverySet);
+    	Iterator<Delivery> iterator = treeSet.iterator();
     	
-    	if( deliveredList == null || deliveredList.isEmpty() )
+    	while( amt_delivered.compareTo(BigDecimal.ZERO)>0 && iterator.hasNext() )
     	{
-    		Set<Delivery> deliverySet = entity.getDelivery();
-            TreeSet<Delivery> treeSet = new TreeSet<>(deliverySet);
-            
-            BigDecimal amt_delivered = BigDecimal.ZERO;
-            BigDecimal amt_remaining = BigDecimal.ZERO;
-            
-            Measure delivered = new Measure();
-            delivered.setValue(amt_delivered);
-            delivered.setUnit(unitEntity);
-            
-            for( Delivery delivery : treeSet )
-            {
-            	Measure quantityDelivery = delivery.getQuantity();
-            	
-            	delivery.setRemaining(quantityDelivery);
-            	delivery.setDelivered(delivered);
-            	
-            	amt_remaining = amt_remaining.add(quantityDelivery.getValue());
-            }
-            
-            Measure remaining = new Measure();
-            remaining.setValue(amt_remaining);
-            remaining.setUnit(unitEntity);
-            
-            entity.setDelivered(delivered);
-            entity.setRemaining(remaining);
-            
+    		Delivery delivery = iterator.next();
+    		
+    		BigDecimal quantityDelivery = delivery.getQuantity().getValue();
+    		Measure measure = new Measure();
+    		measure.setValue(quantityDelivery.min(amt_delivered));
+    		measure.setUnit(unitEntity);
+    		delivery.setDelivered(measure);
+    		
+    		amt_delivered = amt_delivered.subtract(quantityDelivery.min(amt_delivered));
     	}
-    	else
-    	{
-    		// Calculate total amount of delivered quantity
-        	BigDecimal amt_delivered = BigDecimal.ZERO;
-        	for (int i = 0; i < deliveredList.size(); i++) {
-        		BigDecimal quantityValuesOFDelivered = deliveredList.get(i).getQuantity().getValue();
-        		amt_delivered = amt_delivered.add(quantityValuesOFDelivered);
-    		}
-        	
-        	Measure measureEntity = new Measure();
-        	measureEntity.setValue(amt_delivered);
-        	measureEntity.setUnit(unitEntity);
-        	entity.setDelivered(measureEntity);
-        	
-        	BigDecimal amt_remaining = BigDecimal.ZERO;
-            Set<Delivery> deliverySet = entity.getDelivery();
-            TreeSet<Delivery> treeSet = new TreeSet<>(deliverySet);
-            
-            for( Delivery delivery : treeSet ) 
-            {
-            	BigDecimal quantityDelivery = delivery.getQuantity().getValue();
-            	
-            	int result = quantityDelivery.compareTo(amt_delivered);
-            	if (result == -1) 
-            	{
-            		BigDecimal remaining = BigDecimal.ZERO;  
-            		BigDecimal delivered = quantityDelivery;
-    				amt_delivered = amt_delivered.subtract(quantityDelivery);
-    				
-    				Measure measure1 = new Measure();
-    				measure1.setValue(remaining);
-    				measure1.setUnit(unitEntity);
-    				delivery.setRemaining(measure1);
-    				
-    				Measure measure = new Measure();
-                	measure.setValue(delivered);
-                	measure.setUnit(unitEntity);
-    				delivery.setDelivered(measure);
-    			}
-            	else if (result == 1)
-            	{
-            		BigDecimal remaining = quantityDelivery.subtract(amt_delivered);
-            		BigDecimal delivered = quantityDelivery.subtract(remaining);
-            		amt_delivered = BigDecimal.ZERO;
-            		
-                	Measure measure = new Measure();
-                	measure.setValue(remaining);
-                	measure.setUnit(unitEntity);
-                	delivery.setRemaining(measure);
-                	
-                	Measure measure1 = new Measure();
-                	measure1.setValue(delivered);
-                	measure1.setUnit(unitEntity);
-                	delivery.setDelivered(measure1);
-    			}
-            	else if (result == 0) 
-            	{
-            		BigDecimal delivered = amt_delivered;
-            		BigDecimal remaining = quantityDelivery.subtract(delivered);
-            		amt_delivered = BigDecimal.ZERO;
-            		
-                	Measure measure = new Measure();
-                	measure.setValue(delivered);
-                	measure.setUnit(unitEntity);
-    				delivery.setDelivered(measure);
-    				
-    				Measure measure1 = new Measure();
-                	measure1.setValue(remaining);
-                	measure1.setUnit(unitEntity);
-    				delivery.setRemaining(measure1);
-    			}
-            	
-            	BigDecimal remainingQuantity = delivery.getRemaining().getValue();
-                amt_remaining = amt_remaining.add(remainingQuantity);
-            }
-            
-            entity.setDelivery(treeSet);
-            
-            Measure measureEntity1 = new Measure(); 
-        	measureEntity1.setValue(amt_remaining);
-        	measureEntity1.setUnit(unitEntity);
-        	entity.setRemaining(measureEntity1);
-    	}
+    	
+    	entity.setDelivery(treeSet);
     	
     }
 }

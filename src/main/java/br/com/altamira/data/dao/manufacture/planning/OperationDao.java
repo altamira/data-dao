800d4494d6eb69d584016562263ee72ceba19bec
parthpatel-tatvasoft <parth.patel@tatvasoft.com>
2015-03-27 16:23:27 +0530

    ALTAMIRA-157: Manufacture Planning - list Operations

--- src/main/java/br/com/altamira/data/dao/manufacture/planning/OperationDao.java
+++ src/main/java/br/com/altamira/data/dao/manufacture/planning/OperationDao.java
@@ -5,13 +5,8 @@
  */
 package br.com.altamira.data.dao.manufacture.planning;
 
-import br.com.altamira.data.dao.BaseDao;
-import static br.com.altamira.data.dao.Dao.PAGE_SIZE_VALIDATION;
-import static br.com.altamira.data.dao.Dao.PARAMETER_VALIDATION;
-import static br.com.altamira.data.dao.Dao.START_PAGE_VALIDATION;
-import br.com.altamira.data.model.manufacture.planning.BOM;
-import br.com.altamira.data.model.manufacture.planning.Operation;
 import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -20,6 +15,18 @@ import javax.validation.ConstraintViolationException;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.core.MultivaluedMap;
+
+import br.com.altamira.data.dao.BaseDao;
+import br.com.altamira.data.model.manufacture.planning.BOM;
+import br.com.altamira.data.model.manufacture.planning.BOM_;
+import br.com.altamira.data.model.manufacture.planning.Component;
+import br.com.altamira.data.model.manufacture.planning.Component_;
+import br.com.altamira.data.model.manufacture.planning.Item;
+import br.com.altamira.data.model.manufacture.planning.Item_;
+import br.com.altamira.data.model.manufacture.planning.Operation;
+import br.com.altamira.data.model.manufacture.planning.Operation_;
+import br.com.altamira.data.model.manufacture.process.Process;
+import br.com.altamira.data.model.manufacture.process.Process_;
 
 /**
  *
@@ -46,17 +53,31 @@ public class OperationDao extends BaseDao<Operation> {
         //  MN_BOM 
         //    INNER JOIN MN_BOM_ITEM ON MN_BOM.ID = MN_BOM_ITEM.BOM
         //    INNER JOIN MN_BOM_ITEM_CMP ON MN_BOM_ITEM.ID = MN_BOM_ITEM_CMP.ITEM
-        //    INNER JOIN MN_ORDER_ITEM_CMP ON MN_BOM_ITEM_CMP.ID = MN_ORDER_ITEM_CMP.COMPONENT
+        //    --INNER JOIN MN_ORDER_ITEM_CMP ON MN_BOM_ITEM_CMP.ID = MN_ORDER_ITEM_CMP.COMPONENT
         //    INNER JOIN SL_COMPONENT ON MN_BOM_ITEM_CMP.MATERIAL = SL_COMPONENT.ID
         //    INNER JOIN MN_PROCESS ON SL_COMPONENT.PROCESS = MN_PROCESS.ID
         //    INNER JOIN MN_PROCESS_OPERATION ON MN_PROCESS_OPERATION.PROCESS = MN_PROCESS.ID
         //    INNER JOIN MN_OPERATION ON MN_PROCESS_OPERATION.OPERATION = MN_OPERATION.ID;
   
         CriteriaQuery<Operation> criteriaQuery = cb.createQuery(Operation.class);
+        
+        Root<BOM> bom = criteriaQuery.from(BOM.class);
+        Root<Item> item = criteriaQuery.from(Item.class);
+        Root<Component> component = criteriaQuery.from(Component.class);
+        Root<br.com.altamira.data.model.sales.Component> salesComponent = criteriaQuery.from(br.com.altamira.data.model.sales.Component.class);
+        Root<Process> process = criteriaQuery.from(Process.class);
+        Root<br.com.altamira.data.model.manufacture.process.Operation> processOperation = criteriaQuery.from(br.com.altamira.data.model.manufacture.process.Operation.class);
         Root<Operation> operation = criteriaQuery.from(Operation.class);
-
-        criteriaQuery.select(operation);
-
+        
+        criteriaQuery.select(operation).distinct(true);
+        
+        criteriaQuery.where(cb.equal(bom.get(BOM_.id), item.get(Item_.bom)),
+        		cb.equal(item.get(Item_.id), component.get(Component_.item)),
+        		cb.equal(component.get(Component_.material), salesComponent.get(br.com.altamira.data.model.sales.Component_.id)),
+        		cb.equal(salesComponent.get(br.com.altamira.data.model.sales.Component_.process), process.get(Process_.id)),
+        		cb.equal(processOperation.get(br.com.altamira.data.model.manufacture.process.Operation_.process), process.get(Process_.id)),
+        		cb.equal(processOperation.get(br.com.altamira.data.model.manufacture.process.Operation_.operation), operation.get(Operation_.id)));
+        
         return criteriaQuery;
     }
 

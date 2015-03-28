6d1c62ec8654a8f93aa0cc3f62aa66ff445d2bf6
parthpatel-tatvasoft <parth.patel@tatvasoft.com>
2015-03-28 15:00:31 +0530

    ALTAMIRA-159: Manufacture Planning - list BOM's Item

--- src/main/java/br/com/altamira/data/dao/manufacture/planning/OperationDao.java
+++ src/main/java/br/com/altamira/data/dao/manufacture/planning/OperationDao.java
@@ -5,6 +5,8 @@
  */
 package br.com.altamira.data.dao.manufacture.planning;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 import javax.ejb.Stateless;
@@ -152,5 +154,79 @@ public class OperationDao extends BaseDao<Operation> {
                 .setFirstResult(startPage * pageSize)
                 .setMaxResults(pageSize == 0 ? Integer.MAX_VALUE : pageSize)
                 .getResultList();
-    }      
+    }
+    
+  //ALTAMIRA-159 : Manufacture Planning - list BOM's Item
+    /**
+    *
+    * @param parameters
+    * @return
+    */
+   public CriteriaQuery<Item> getBOMItemQuery(@NotNull MultivaluedMap<String, String> parameters) {
+       CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+
+       // TODO: 
+       // ALTAMIRA-159:
+       // ------------------------------------------------------------------------------------
+       //SELECT DISTINCT 
+       //MN_BOM_ITEM.ID, MN_BOM_ITEM.ITEM, MN_BOM_ITEM.DESCRIPTION
+       //FROM 
+       //  MN_BOM 
+       //    INNER JOIN MN_BOM_ITEM ON MN_BOM.ID = MN_BOM_ITEM.BOM
+       //    INNER JOIN MN_BOM_ITEM_CMP ON MN_BOM_ITEM.ID = MN_BOM_ITEM_CMP.ITEM
+       //    --INNER JOIN MN_ORDER_ITEM_CMP ON MN_BOM_ITEM_CMP.ID = MN_ORDER_ITEM_CMP.COMPONENT
+       //    INNER JOIN SL_COMPONENT ON MN_BOM_ITEM_CMP.MATERIAL = SL_COMPONENT.ID
+       //    INNER JOIN MN_PROCESS ON SL_COMPONENT.PROCESS = MN_PROCESS.ID
+       //    INNER JOIN MN_PROCESS_OPERATION ON MN_PROCESS_OPERATION.PROCESS = MN_PROCESS.ID
+       //    INNER JOIN MN_OPERATION ON MN_PROCESS_OPERATION.OPERATION = MN_OPERATION.ID
+       //WHERE
+       //  MN_OPERATION.ID = 10500
+       //  AND MN_BOM.ID = 61849;
+ 
+       CriteriaQuery<Item> criteriaQuery = cb.createQuery(Item.class);
+       Root<BOM> bom = criteriaQuery.from(BOM.class);
+       Root<Item> item = criteriaQuery.from(Item.class);
+       Root<Component> component = criteriaQuery.from(Component.class);
+       Root<br.com.altamira.data.model.sales.Component> salesComponent = criteriaQuery.from(br.com.altamira.data.model.sales.Component.class);
+       Root<Process> process = criteriaQuery.from(Process.class);
+       Root<br.com.altamira.data.model.manufacture.process.Operation> processOperation = criteriaQuery.from(br.com.altamira.data.model.manufacture.process.Operation.class);
+       Root<Operation> operation = criteriaQuery.from(Operation.class);
+       
+       criteriaQuery.select(cb.construct(Item.class, 
+       		item.get(Item_.id),
+       		item.get(Item_.item),
+       		item.get(Item_.description)));
+       
+       criteriaQuery.where(cb.equal(bom.get(BOM_.id), item.get(Item_.bom)),
+    		   cb.equal(item.get(Item_.id), component.get(Component_.item)),
+    		   cb.equal(component.get(Component_.material), salesComponent.get(br.com.altamira.data.model.sales.Component_.id)),
+    		   cb.equal(salesComponent.get(br.com.altamira.data.model.sales.Component_.process), process.get(Process_.id)),
+    		   cb.equal(processOperation.get(br.com.altamira.data.model.manufacture.process.Operation_.process), process.get(Process_.id)),
+    		   cb.equal(processOperation.get(br.com.altamira.data.model.manufacture.process.Operation_.operation), operation.get(Operation_.id)),
+    		   cb.equal(operation.get(Operation_.id), parameters.get("id").get(0)),
+    		   cb.equal(bom.get(BOM_.id), parameters.get("id").get(1)));
+       
+       return criteriaQuery;
+   }
+    
+    /**
+    *
+    * @param parameters
+    * @param startPage
+    * @param pageSize
+    * @return
+    */
+   public List<Item> listBOMItem(
+           @NotNull(message = PARAMETER_VALIDATION) MultivaluedMap<String, String> parameters,
+           @Min(value = 0, message = START_PAGE_VALIDATION) int startPage,
+           @Min(value = 0, message = PAGE_SIZE_VALIDATION) int pageSize)
+           throws ConstraintViolationException {
+
+	   List<Item> item = entityManager.createQuery(this.getBOMItemQuery(parameters))
+               .setFirstResult(startPage * pageSize)
+               .setMaxResults(pageSize == 0 ? Integer.MAX_VALUE : pageSize)
+               .getResultList();
+       
+       return new ArrayList<Item>(new LinkedHashSet<Item>(item));
+   }
 }

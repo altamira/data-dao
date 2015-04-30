237c45ef32b9a532d487b58a5b58fc26cc3a5c33
parthpatel-tatvasoft <parth.patel@tatvasoft.com>
2015-04-30 12:30:11 +0530

    ALTAMIRA-223: Manufacture operation - search parameter not working

--- src/main/java/br/com/altamira/data/dao/manufacture/OperationDao.java
+++ src/main/java/br/com/altamira/data/dao/manufacture/OperationDao.java
@@ -7,7 +7,14 @@ package br.com.altamira.data.dao.manufacture;
 
 import br.com.altamira.data.dao.BaseDao;
 import br.com.altamira.data.model.manufacture.Operation;
+import br.com.altamira.data.model.manufacture.Operation_;
+
 import javax.ejb.Stateless;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.core.MultivaluedMap;
 
 /**
  *
@@ -15,5 +22,29 @@ import javax.ejb.Stateless;
  */
 @Stateless(name = "br.com.altamira.data.dao.manufacture.OperationDao")
 public class OperationDao extends BaseDao<Operation> {
+	
+	// ALTAMIRA-223: Manufacture operation - search parameter not working
+	@Override
+	public CriteriaQuery<Operation> getCriteriaQuery(@NotNull MultivaluedMap<String, String> parameters) {
+	
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Operation> criteriaQuery = cb.createQuery(Operation.class);
+        Root<Operation> entity = criteriaQuery.from(Operation.class);
+
+        criteriaQuery.select(entity);
+
+        if (parameters.get("search") != null
+                && !parameters.get("search").isEmpty()
+                && !parameters.get("search").get(0).isEmpty()) {
+            String searchCriteria = "%" + parameters.get("search").get(0)
+                    .toLowerCase().trim() + "%";
+
+            criteriaQuery.where(cb.or(
+                    cb.like(cb.lower(entity.get(Operation_.name).as(String.class)), searchCriteria),
+                    cb.like(cb.lower(entity.get(Operation_.description).as(String.class)), searchCriteria)));
+        }
+
+        return criteriaQuery;
+	}
     
 }
